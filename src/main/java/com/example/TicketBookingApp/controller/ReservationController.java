b0c7406adf590f3891b8c51d470aea55e7b37c67
kkalkowski96 <krzysztof.kalkowski96@gmail.com>
2021-09-22 09:27:55 +0200

    Corrected RequestMapping in ReservationController

--- src/main/java/com/example/TicketBookingApp/controller/ReservationController.java
+++ src/main/java/com/example/TicketBookingApp/controller/ReservationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("reservation")
+@RequestMapping("/reservation")
 @RequiredArgsConstructor
 public class ReservationController {
 
